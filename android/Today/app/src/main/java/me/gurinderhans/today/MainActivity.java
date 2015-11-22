101057076239ccbd47b34fbb55d0dc4b9de5f63f
Gurinder Hans <hello@gurinderhans.me>
2015-11-21 23:34:30 -0800

    added google analytics

--- android/Today/app/src/main/java/me/gurinderhans/today/MainActivity.java
+++ android/Today/app/src/main/java/me/gurinderhans/today/MainActivity.java
@@ -3,10 +3,15 @@ package me.gurinderhans.today;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    private Tracker mTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager pager = (ViewPager) findViewById(R.id.today_pager);
         pager.setAdapter(new TodayPagerAdapter(getSupportFragmentManager()));
+
+        AnalyticsApplication application = (AnalyticsApplication) getApplication();
+        mTracker = application.getDefaultTracker();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String name = "MainActivity";
+        Log.i(TAG, "Setting screen name: " + name);
+        mTracker.setScreenName("Image~" + name);
+        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
 }
